e3e2829fb5acbc8a32c45e2c673ada34d653bc71
arocha23 <rochaad02@gmail.com>
2019-01-11 09:14:41 -0500

    Fixed incrementing

--- clickergame.py
+++ clickergame.py
@@ -36,9 +36,13 @@ class Player:
         self.player_attributes['score'] = 0
 
     def get_score(self):
-        return(str(self.player_attributes['score']))
+        return(str(round(self.player_attributes['score'])))
 
-    def inc_score(self):
+    def inc_score_tick(self):
+        self.player_attributes['score'] += 1/15
+        return(self.player_attributes['score'])
+
+    def inc_score_click(self):
         self.player_attributes['score'] += 1
         return(self.player_attributes['score'])
 
@@ -52,8 +56,13 @@ class Upgrade1(Player):
         Player.__init__(self)
     
     def inc_score(self):
-        self.player_attributes['score'] += 10
+        self.player_attributes['score'] += 9
         return(self.player_attributes['score']) 
+
+class Upgrade2(Player):
+    """Second button upgrade"""
+    def __init__(self):
+        Player.__init__(self)
     
 
 def txt_obj(text, font):
@@ -79,34 +88,35 @@ def startButton(x, y, width, height, old_color, new_color):
     else:
         pygame.draw.rect(game_screen, old_color,(x, y, width, height))
 
-def anyOtherButton(x, y, width, height, old_color, new_color, player_object):
-    """Will be used for any other button other than the start button."""
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    pygame.draw.rect(game_screen, old_color,(x, y, width, height))
-    if x + width > mouse[0] > x and  y + height > mouse[1] > y:
-        pygame.draw.rect(game_screen, new_color, (x, y , width, height))
-        if click != None and click[0] == 1:
-            player_object.inc_score()
-            print("Clicks of Productivity: ", player_object.get_score())
-            pygame.display.update()
-            clock.tick(max_fps)
-            
+class Button(pygame.Rect):
+    """Button class for any button after menu screen"""
+    def __init__(self, x, y, width, height, message, old_color, new_color, player_object):
+        super().__init__(x, y, width, height)
+        self.message = message
+        self.old_color = old_color
+        self.new_color = new_color
+        self.player_object = player_object
+    
+    def clicked(self):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        pygame.draw.rect(game_screen, self.old_color, (self.x, self.y, self.width, self.height))
+        if self.x + self.width > mouse[0] > self.x and  self.y + self.height > mouse[1] > self.y:
+            pygame.draw.rect(game_screen, self.new_color, (self.x, self.y , self.width, self.height))
+            if click != None and click[0] == 1:
+                self.player_object.inc_score_click()
+                print("Clicks of Productivity: ", self.player_object.get_score())
+                pygame.display.update()
+                clock.tick(max_fps)
+        else:
+            pygame.draw.rect(game_screen, self.old_color, (self.x, self.y, self.width, self.height))
         
-            # button_text = pygame.font.Font("Quicksand-Regular.ttf", 15)
-            # txt_surface, txt_rect = txt_obj("Clicks of Productivity: " + player_object.get_score(), button_text)
-            # txt_surface, txt_rect = txt_obj("Clicks of Productivity: " + player_object.get_score(), button_text)
-            # txt_rect.center = ((200+(250/2)), (225+(50/2)))
-            # # txt_cover = pygame.Surface((txt_rect[2], txt_rect[3]))
-            # # txt_cover.fill((255, 255, 255))
-            # game_screen.blit(txt_surface, txt_rect)
-            # print("Clicks of Productivity: ", player_object.get_score())
-            # print(txt_rect)
-            # game_screen.blit(display_rdmImg, (x_, y_))
-            # pygame.display.update()
-            # clock.tick(max_fps)
-    else:
-        pygame.draw.rect(game_screen, old_color,(x, y, width, height))
+        button_text = pygame.font.Font("Quicksand-Regular.ttf", 15)
+        txt_surface, txt_rect = txt_obj(self.message, button_text)
+        txt_rect.center = ((self.x + (self.width/2)), (self.y + (self.height/2)))
+        game_screen.blit(txt_surface, txt_rect)
+
+
 
 
 def game_intro():
@@ -149,17 +159,6 @@ def game_intro():
         clock.tick(max_fps)
 
 
-# class Status:
-#     def __init__(self, productivity, increment):
-#         self.productivity = productivity
-#         self.increment = increment
-#         self.start_time = time.time()
-    
-#     def is_clicked(self):
-#         for event in pygame.event.get():
-#             if event.type == pygame.KEYDOWN:
-#                 
-
 
 def start_game():
     """Running game, WIP"""
@@ -175,23 +174,20 @@ def start_game():
         
         game_screen.blit(background_image, [0, 0])
                 
-
-        anyOtherButton(200, 300, 250, 50, cool_blue, cooler_blue, initial_button)
-        anyOtherButton(300, 400, 250, 50, cool_blue, cooler_blue, button1)
         button_text = pygame.font.Font("Quicksand-Regular.ttf", 15)
-        initial_buttontxt_surface, initial_buttontxt_rect = txt_obj("Click to increase productivity!", button_text)
-        initial_buttontxt_rect.center = ((200+(250/2)), (300+(50/2)))
-        button1txt_surface, button1txt_rect = txt_obj("Buy a mom", button_text)
+        prod_button = Button(200, 300, 250, 50, "Click to increase productivity!", cool_blue, cooler_blue, initial_button)
+        upgrade1 = Button(300, 400, 250, 50, "Buy something", cool_blue, cooler_blue, button1)
+        prod_button.clicked()
+        upgrade1.clicked()
         txt_surface, txt_rect = txt_obj("Clicks of Productivity: " + initial_button.get_score(), button_text)
         txt_rect.center = ((200+(250/2)), (225+(50/2)))
-        # txt_cover = pygame.Surface((txt_rect[2], txt_rect[3]))
-        # txt_cover.fill((255, 255, 255))
-        game_screen.blit(initial_buttontxt_surface, initial_buttontxt_rect)
         game_screen.blit(txt_surface, txt_rect)
-        initial_button.inc_score() 
+        initial_button.inc_score_tick() 
         pygame.display.update()
         clock.tick(15)
         
 game_intro()
 start_game()
 pygame.quit()
+
+# Seize the means of production
